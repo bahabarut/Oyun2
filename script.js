6195e0cc49d796e88c29c59a467128407ee19dbc
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2024-04-11 14:39:44 +0300

    devam ediliyor

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+var sayac = 0;
 $(document).ready(function () {
+    HedefleriHazirla();
     for (var i = 0; i < 40; i++) {
         var line = document.createElement("span");
         line.setAttribute("class", "barLines");
@@ -7,7 +9,27 @@ $(document).ready(function () {
 });
 $(document).on("keydown", function (ev) {
     if (ev.which === 32) {
-        var val = $("#bar").val();
-        $("#bar").val(parseInt(val)+1);
+        console.log(sayac +=2);
     }
 });
+$(document).on("keyup", function (ev) {
+    if (ev.which === 32) {
+        sayac = 0;
+    }
+});
+
+function HedefleriHazirla() {
+    var alan = $("#hedef");
+    for (var i = 0; i < 7; i++) {
+        var newEl = document.createElement("span");
+        newEl.setAttribute("class", "hedefler");
+
+        newEl.style.top = `${RandomUret(1, 94)}%`
+        newEl.style.left = `${RandomUret(1, 95)}%`
+
+        alan.append(newEl);
+    }
+}
+function RandomUret(min, max) {
+    return Math.floor(min + Math.random() * max);
+}
